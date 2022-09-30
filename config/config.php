72a019c35c069bcd22fe7b26ce66de50ec271fc0
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2022-09-29 21:01:29 -0500

    Arreglar conexion remota

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ class Config
             $HOST = 'us-cdbr-east-06.cleardb.net';
             $USER = 'becceebb7b51b8';
             $PASSWORD = '92e3e329';
-            $conectar = $this->db = new PDO("mysql:local=$HOST;dbname=$NAMEDB", "$USER", "$PASSWORD");
+            $conectar = $this->db = new PDO("mysql:host=$HOST;dbname=$NAMEDB", "$USER", "$PASSWORD");
             return $conectar;
         } catch (Exception $e) {
             print "¡Error BD!: " . $e->getMessage() . "<br/>";
